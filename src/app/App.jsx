11c5b16727d5bdfd7129f2e94564896d55e2778e
German Kosach <fuerstein@yandex.ru>
2023-11-12 17:26:26 +0300

    rebase with git rm -r --cached

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -1,66 +1,54 @@
-import { Routes, Route, BrowserRouter } from "react-router-dom"
-import React from "react"
+// App.jsx
+import React, { useEffect } from "react"
+import { Routes, Route } from "react-router-dom"
+import { connect } from "react-redux"
+import { compose } from "redux"
+import { initializeApp } from "./redux/reducers/appReducer"
+import Preloader from "../features/Preloader/Preloader"
+import HeaderContainer from "../widgets/Header/HeaderContainer"
+import Navbar from "../widgets/Navbar/Navbar"
+import Footer from "../widgets/Footer/Footer"
 import News from "../pages/News/News"
-
 import Music from "../pages/Music/Music"
 import Settings from "../pages/Settings/Settings"
 import DialogsContainer from "../pages/Dialogs/DialogsContainer"
 import UsersContainer from "../pages/Users/UsersContainer"
 import ProfileContainer from "../pages/Profile/ProfileContainer"
 import LoginPage from "../pages/Login/Login"
-import HeaderContainer from "../widgets/Header/HeaderContainer"
-import Navbar from "../widgets/Navbar/Navbar"
-import Footer from "../widgets/Footer/Footer"
-import { Provider, connect } from "react-redux"
-import { initializeApp } from "./redux/reducers/appReducer"
-import "./styles/App.css"
-import Preloader from "../features/Preloader/Preloader"
-import store from "./redux/redux-store/redux-store"
-import { compose } from "redux"
 
-class App extends React.Component {
-  componentDidMount() {
-    this.props.initializeApp()
+const App = ({ initialization, initializeApp }) => {
+  useEffect(() => {
+    initializeApp()
+  }, [initializeApp])
+
+  if (!initialization) {
+    return <Preloader />
   }
-  render() {
-    if (!this.props.initialization) {
-      return <Preloader />
-    }
 
-    return (
-      <div className='app-wrapper'>
-        <HeaderContainer />
-        <Navbar />
-        <div className='app-wrapper-content'>
-          <Routes>
-            <Route path='/login' element={<LoginPage />} />
-            <Route path='/dialogs' element={<DialogsContainer />} />
-            <Route path='/profile/:userId?' element={<ProfileContainer />} />
-            <Route path='/users' element={<UsersContainer />} />
-            <Route path='/news' element={<News />} />
-            <Route path='/music' element={<Music />} />
-            <Route path='/settings' element={<Settings />} />
-          </Routes>
-        </div>
-        <Footer />
+  return (
+    <div className='app-wrapper'>
+      <HeaderContainer />
+      <Navbar />
+      <div className='app-wrapper-content'>
+        <Routes>
+          <Route path='/login' element={<LoginPage />} />
+          <Route path='/dialogs' element={<DialogsContainer />} />
+          <Route path='/profile/:userId?' element={<ProfileContainer />} />
+          <Route path='/users' element={<UsersContainer />} />
+          <Route path='/news' element={<News />} />
+          <Route path='/music' element={<Music />} />
+          <Route path='/settings' element={<Settings />} />
+        </Routes>
       </div>
-    )
-  }
+      <Footer />
+    </div>
+  )
 }
 
 const mapStateToProps = (state) => ({
   initialization: state.app.initialization,
 })
 
-let AppContainer = compose(connect(mapStateToProps, { initializeApp })(App))
+const AppContainer = compose(connect(mapStateToProps, { initializeApp }))(App)
 
-let MainApp = (props) => {
-  return (
-    <BrowserRouter>
-      <Provider store={store}>
-        <AppContainer store={store} />
-      </Provider>
-    </BrowserRouter>
-  )
-}
-export default MainApp
+export default AppContainer
--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,19 @@
 import React from "react"
 import ReactDOM from "react-dom"
-import { createRoot } from "react-dom"
+import { BrowserRouter } from "react-router-dom" // Import BrowserRouter
+import { createRoot } from "react-dom/client"
 import MainApp from "./app/App"
 import store from "./app/redux/redux-store/redux-store"
 import "./app/styles/reset.css"
 import "./app/styles/fonts.css"
+import { Provider } from "react-redux"
 
 const rootElement = document.getElementById("root")
 
-createRoot(rootElement).render(<MainApp store={store} />)
+createRoot(rootElement).render(
+  <Provider store={store}>
+    <BrowserRouter>
+      <MainApp />
+    </BrowserRouter>
+  </Provider>
+)
