d8b17ccbe096cdde1f1a6491ebbf737de46f40f6
German Kosach <fuerstein@yandex.ru>
2023-11-12 17:27:53 +0300

    rebase with git rm -r --cached

--- src/main.jsx
+++ src/main.jsx
@@ -1,19 +1,10 @@
-import React from "react"
-import ReactDOM from "react-dom"
-import { BrowserRouter } from "react-router-dom" // Import BrowserRouter
-import { createRoot } from "react-dom/client"
+import ReactDOM from "react-dom/client"
+// import App from "@/app/App"
+import store from "@/app/redux/redux-store/redux-store.js"
+import "@/app/styles/reset.css"
+import "@/app/styles/fonts.css"
 import MainApp from "./app/App"
-import store from "./app/redux/redux-store/redux-store"
-import "./app/styles/reset.css"
-import "./app/styles/fonts.css"
-import { Provider } from "react-redux"
 
-const rootElement = document.getElementById("root")
-
-createRoot(rootElement).render(
-  <Provider store={store}>
-    <BrowserRouter>
-      <MainApp />
-    </BrowserRouter>
-  </Provider>
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <MainApp store={store} />
 )
