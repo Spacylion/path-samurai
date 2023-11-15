85a8823fc215a01274740287dfdabaf9565f2018
German Kosach <fuerstein@yandex.ru>
2023-11-15 21:13:12 +0300

    users logic fixed, pagination styling

--- src/main.jsx
+++ src/main.jsx
@@ -1,4 +1,5 @@
 import * as serviceWorker from "./serviceWorker"
+import React from "react"
 import { createRoot } from "react-dom/client"
 import MainApp from "./app/App"
 import "@/app/styles/reset.css"
